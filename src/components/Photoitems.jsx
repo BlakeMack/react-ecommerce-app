f9b51b6d7d06c34c0adf1b43e0653c531132e90b
Blake McKenzie <blakemckenzie365@gmail.com>
2022-12-28 15:09:51 +0000

    cart and heart icons hover show only property added

--- src/components/Photoitems.jsx
+++ src/components/Photoitems.jsx
@@ -1,9 +1,20 @@
+import { useState } from "react"
+
 function Photoitems ({img, className}) {
+
+  const [hovered, setHovered] = useState(false)
+  const hearticon = hovered && <i className="ri-heart-line heart"></i>
+  const carticon = hovered && <i className="ri-shopping-cart-2-line cart"></i>
   return (
-    <div className={`image-container ${className}`}>
+
+
+    <div className={`image-container ${className}`}
+          onMouseEnter={() => setHovered(true)}
+          onMouseLeave={() => setHovered(false)}
+    >
       <img src={img.url} alt="" className='image-grid'/>
-      <i className="ri-shopping-cart-2-line cart"></i>
-      <i className="ri-heart-line heart"></i>
+      {hearticon}
+      {carticon}
     </div>
   )
 }
