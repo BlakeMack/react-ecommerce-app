a179a406f8f29929b1e0b03f0221b6771b69983f
Blake McKenzie <blakemckenzie365@gmail.com>
2022-12-28 17:35:15 +0000

    header component shows dynamic icons if cart has items or is empty

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,29 @@
 import {Link} from "react-router-dom"
+import { useContext } from "react"
+import { Context } from "../context"
 
-function App () {
+function Header () {
+  const {cart} = useContext(Context)
+
+  const itemsInCart = cart.length > 0
+
+  const displayCartIcon = () => {
+    if(itemsInCart) {
+      return (
+        <Link to="/cart"><i className="ri-shopping-cart-fill ri-fw ri-5x"></i></Link>
+      )
+    } else {
+      return (
+        <Link to="/cart"><i className="ri-shopping-cart-line ri-fw ri-5x"></i></Link>
+      )
+    }
+  }
   return (
     <div className="navbar">
         <Link to="/"><h1>Pic Some</h1></Link>
-        <Link to="/cart"><i className="ri-shopping-cart-line ri-fw ri-5x"></i></Link>
+        {displayCartIcon()}
     </div>
   )
 }
 
-export default App
+export default Header
